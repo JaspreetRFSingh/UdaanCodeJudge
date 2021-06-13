97d63571578de2b31645efa17440e9c7f67b61a7
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-06-13 13:12:30 +0530

    added quizQuestion service and its implementaton

---
+++ WebApplication2/Services/QuizQuestionService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Services
+{
+    public class QuizQuestionService : IQuizQuestionService
+    {
+        
+        private readonly UdaanDemoDBContext _context;
+
+        public QuizQuestionService(UdaanDemoDBContext context)
+        {
+            _context = context;
+        }
+        public bool CreateQuizQuestionRecord(int quizId, int questionId)
+        {
+            try
+            {
+                var quizQuestion = new QuizQuestion
+                {
+                    QuestionId = questionId,
+                    QuizId = quizId
+                };
+                _context.QuizQuestions.Add(quizQuestion);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}

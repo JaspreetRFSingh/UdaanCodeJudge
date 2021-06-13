bee8fce6e78670078f1640af63552ff6ad36c65c
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-06-13 13:11:10 +0530

    Created QuestionService

---
+++ WebApplication2/Services/IQuestionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using WebApplication2.Models;
+
+namespace WebApplication2.Services
+{
+    public interface IQuestionService
+    {
+        public List<Question> GetAllQuestions();
+        public List<Question> GetQuestionByQuiz(int quizId);
+        public bool AddQuestion(Question question);
+    }
+}
---
+++ WebApplication2/Services/QuestionService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        
+        private readonly UdaanDemoDBContext _context;
+
+        public QuestionService(UdaanDemoDBContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddQuestion(Question question)
+        {
+            try
+            {
+                _context.Questions.Add(question);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public List<Question> GetAllQuestions()
+        {
+            return _context.Questions.ToList<Question>();
+        }
+
+        public List<Question> GetQuestionByQuiz(int quizId)
+        {
+            throw new NotImplementedException();
+        }
+        
+
+    }
+}

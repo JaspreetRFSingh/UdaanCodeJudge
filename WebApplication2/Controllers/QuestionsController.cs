95ed30e7bb0a8cb9cdb65538f44c77f1640f2ceb
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-06-13 13:11:51 +0530

    Added controller class for the ..../questions

---
+++ WebApplication2/Controllers/QuestionsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+using WebApplication2.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        
+        private readonly IQuestionService _service;
+
+        public QuestionsController(IQuestionService service)
+        {
+            _service = service;
+        }
+        // GET: api/<QuestionsController>
+        [HttpGet]
+        public ActionResult<List<Question>> GetQuestions()
+        {
+            return _service.GetAllQuestions();
+        }
+
+        // GET api/<QuestionsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<QuestionsController>
+        [HttpPost]
+        public ActionResult PostQuestion([FromBody] Question question)
+        {
+            var insertFlag = _service.AddQuestion(question);
+            if (insertFlag)
+            {
+                return Ok(question);
+            }
+            else
+            {
+                return BadRequest(question);
+            }
+        }
+
+        // PUT api/<QuestionsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<QuestionsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        
+    }
+}
